4679578d6991988e746bcbf71ae345ae23c856da
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-06-02 05:53:34 +0000

    2回目のコミット

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require jquery.raty.js
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -14,6 +14,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
+window.$ = window.jQuery = require('jquery');   
 
 Rails.start()
 Turbolinks.start()
