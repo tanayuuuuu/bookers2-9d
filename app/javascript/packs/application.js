f005ea99b11eca5fd5859efbd13c432a04f45b1d
tanayuuuuu <101308377+tanayuuuuu@users.noreply.github.com>
2022-06-05 08:07:21 +0000

    initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,20 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery.raty.js
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@fortawesome/fontawesome-free/js/all'
 
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
-window.$ = window.jQuery = require('jquery');   
+import "jquery.raty.js"
+window.$ = window.jQuery = require('jquery');
 
+require('packs/raty')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
